7e218fa0a01422b835d88cbef173afd7b3d721ad
Khushi Rajpurohit <rajpurohitkhushi36@gmail.com>
2024-02-27 18:55:28 +0530

    edited package.json to deploy

--- frontend/src/components/suggestPlants.js
+++ frontend/src/components/suggestPlants.js
@@ -81,6 +81,7 @@ export default function SuggestPlant() {
         alt={suggestedPlant.image}
         className=" image"
       />
+      
     )}
                    </div>
                    <div className="mt-4  ">
